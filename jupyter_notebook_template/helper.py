ddebfdc1a5db8d4c449c6f4250d256c0dae658e1
Raj Gupta <rajkumargupta1@gmail.com>
2019-12-02 11:39:26 +0530

    EDA helper

---
+++ jupyter_notebook_template/helper.py
@@ -0,0 +1,80 @@
+'''
+Helper functions to do EDA
+'''
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy import stats
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+
+def categorical_summarized(dataframe, x=None, y=None, hue=None, palette='Set1', ax=None, order=None, verbose=True):
+    '''
+    Helper function that gives a quick summary of a given column of categorical data
+
+    Arguments
+    =========
+    dataframe: pandas dataframe
+    x: str. horizontal axis to plot the labels of categorical data, y would be the count
+    y: str. vertical axis to plot the labels of categorical data, x would be the count
+    hue: str. if you want to compare it another variable (usually the target variable)
+    palette: array-like. Colour of the plot
+
+    Returns
+    =======
+    Quick Stats of the data and also the count plot
+    '''
+    if x == None:
+        column_interested = y
+    else:
+        column_interested = x
+    series = dataframe[column_interested]
+    print(series.describe())
+    print('mode: ', series.mode())
+    if verbose:
+        print('='*80)
+        print(series.value_counts())
+
+    sns.countplot(x=x, y=y, hue=hue, data=dataframe,
+                  palette=palette, order=order, ax=ax)
+    plt.show()
+
+
+def quantitative_summarized(dataframe, x=None, y=None, hue=None, palette='Set1', ax=None, order=None, verbose=True, swarm=False):
+    '''
+    Helper function that gives a quick summary of quantattive data
+
+    Arguments
+    =========
+    dataframe: pandas dataframe
+    x: str. horizontal axis to plot the labels of categorical data (usually the target variable)
+    y: str. vertical axis to plot the quantitative data
+    hue: str. if you want to compare it another categorical variable (usually the target variable if x is another variable)
+    palette: array-like. Colour of the plot
+    swarm: if swarm is set to True, a swarm plot would be overlayed
+
+    Returns
+    =======
+    Quick Stats of the data and also the box plot of the distribution
+    '''
+    series = dataframe[y]
+    print(series.describe())
+    print('mode: ', series.mode())
+    if verbose:
+        print('='*80)
+        print(series.value_counts())
+
+    sns.boxplot(x=x, y=y, hue=hue, data=dataframe,
+                palette=palette, order=order, ax=ax)
+
+    if swarm:
+        sns.swarmplot(x=x, y=y, hue=hue, data=dataframe,
+                      palette=palette, order=order, ax=ax)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    pass
